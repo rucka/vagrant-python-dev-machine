acaaed2f54fda8b115f30a115eca2b83e5d140fe
rucka <gianluca@carucci.org>
2014-06-01 16:14:55 +0200

    added sample python class and unit test

--- manifests/vagrant.pp
+++ manifests/vagrant.pp
@@ -2,6 +2,7 @@ $PROJ_DIR = "/home/vagrant/src"
 
 info("started vagrant provisioning")
 info("project dir is $PROJ_DIR")
+info("host is ${fqdn}")
 
 Exec {
     path => "/usr/local/bin:/usr/bin:/usr/sbin:/bin",
