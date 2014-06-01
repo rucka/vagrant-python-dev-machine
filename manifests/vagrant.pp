2b2a086644b9556db0e143726ceeea9d0088f7fc
rucka <gianluca@carucci.org>
2014-06-01 15:07:27 +0200

    configure puppet to download latest python packages

--- manifests/vagrant.pp
+++ manifests/vagrant.pp
@@ -3,4 +3,19 @@ $PROJ_DIR = "/home/vagrant/src"
 info("started vagrant provisioning")
 info("project dir is $PROJ_DIR")
 
+class init {
+    exec { "update-apt":
+        command => "sudo apt-get update",
+    }
+
+    package {
+        ["python", "python-dev", "python-pip"]:
+        ensure => latest,
+        require => Exec['updage-apt']
+    }
+}
+
+include init
+
+
 info("stopped vagrant provisioning")
